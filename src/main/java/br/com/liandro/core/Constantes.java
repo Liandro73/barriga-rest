504df610a3b41001aec3a680e79b5b04a1189fa4
Dernival Liandro <liandro.sys@gmail.com>
2022-01-29 18:40:47 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/liandro/core/Constantes.java
@@ -0,0 +1,15 @@
+package br.com.liandro.core;
+
+import io.restassured.http.ContentType;
+
+public interface Constantes {
+
+    String APP_BASE_URL = "https://barrigarest.wcaquino.me";
+    Integer APP_PORT = 443; // http -> 80
+    String APP_BASE_PATH = "";
+
+    ContentType APP_CONTENT_TYPE = ContentType.JSON;
+
+    Long MAX_TIMEOUT = 1000L;
+
+}
