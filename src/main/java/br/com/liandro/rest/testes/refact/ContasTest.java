a1661afd773b646d0002a9c75077d12f377e6e50
Dernival Liandro <liandro.sys@gmail.com>
2022-01-30 17:17:21 -0300

    Testes de contas

--- src/main/java/br/com/liandro/rest/testes/refact/ContasTest.java
+++ src/main/java/br/com/liandro/rest/testes/refact/ContasTest.java
@@ -1,14 +1,17 @@
 package br.com.liandro.rest.testes.refact;
 
+import br.com.liandro.core.BaseTest;
 import io.restassured.RestAssured;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
-public class ContasTest {
+public class ContasTest extends BaseTest {
 
     @BeforeClass
     public static void login() {
@@ -28,6 +31,49 @@ public class ContasTest {
         RestAssured.requestSpecification.header("Authorization", "JWT " + TOKEN);
 
         RestAssured.get("/reset").then().statusCode(200);
+    }
+
+    @Test
+    public void deveIncluirContaComSucesso() {
+        given()
+                .body("{\"nome\": \"nova conta\"}")
+            .when()
+                .post("/contas")
+            .then()
+                .statusCode(201)
+        ;
+    }
+
+    @Test
+    public void deveAlterarContaComSucesso() {
+
+        Integer CONTA_ID = getIdContaPeloNome("Conta para alterar");
+
+        given()
+                .body("{\"nome\": \"conta alterada\"}")
+                .pathParam("id", CONTA_ID)
+            .when()
+                .put("/contas/{id}")
+            .then()
+                .statusCode(200)
+                .body("nome", is("conta alterada"))
+        ;
+    }
+
+    @Test
+    public void naoDeveInserirContaComMesmoNome() {
+        given()
+                .body("{\"nome\": \"Conta mesmo nome\"}")
+            .when()
+                .post("/contas")
+            .then()
+                .statusCode(400)
+                .body("error", is("Já existe uma conta com esse nome!"))
+        ;
+    }
+
+    public Integer getIdContaPeloNome(String nome) {
+        return RestAssured.get("/contas?name=" + nome).then().extract().path("id[0]");
     }
 
 }
